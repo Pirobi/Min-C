b1b886e091e2dad433459ca3fa584e1eaaf5b8c4
Kevin <kevin7571@yahoo.com>
2015-04-24 10:31:14 +0900

    Translated print.ml to print.c and compiled the C code using gcc

---
+++ translation/print.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"csyntax.c"
+
+
+int main(){
+int Ti3 = 123;
+printf("%d", Ti3);
+int Ti4 = 456;
+int Ti5 = -Ti4;
+printf("%d", Ti5);
+int Ti6 = 789;
+int Ti7 = 0;
+int Ti8 = Ti6 + Ti7;
+printf("%d", Ti8);
+int ans = 1;
+return ans;
+}
